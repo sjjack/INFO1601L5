232ad2b77b1084bb2669278e7f066011e5bd4bb4
sjjack <sharonajack04@gmail.com>
2025-03-06 18:49:29 -0800

    prim

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-let radius = 7;
-const pi = 3.14;
+let str1 ="Hello World";
+let str2 = 'Hello World';
+let str3 = `
+    hello 
+    world
+`;// template literal (backtick above tab key)
 
-let area = radius * radius * pi;
+console.log(str1, str2, str3);
 
-console.log(area);
+cout << " Hello world"<< str1 <<endl
+let name = "bob";
+let age = 24;
+
+console.log(typeof(name))//string
+console.log(typeof(age))//number
+
+console.log(`Hello my name is ${name}, I'm ${age} years old`);
+//Hello my name is bob, I'm 24 years old.
+
+console.log(`I was born in $${2020 - age}`)
+//I was born in 1966
