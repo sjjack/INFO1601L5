350600cffbdb7847a49cc14759900465cd69bebd
sjjack <sharonajack04@gmail.com>
2025-03-06 21:35:39 -0800

    ex

--- index.js
+++ index.js
@@ -1,18 +1,64 @@
-function happyPrint(string){
-  console.log("😀: "+string);
- }
- 
- function sadPrint(string){
-  console.log("😢: "+string);
- }
- 
- //This high-order function adds 2 parameters and passes the answer to the callback
- function add(a, b, callback){
-    let ans = a + b;
-    happyPrint(ans)
- }
- 
- //call add passing the callbacks to it
- 
- add(5, 10);
- add(11, 12);
+//object literal
+let bob = {
+  fname: "bob",
+  lname: "smith",
+  age: 18,
+  height: 6,
+  transcript:[
+    {
+      course: 'INFO 1603',
+      grades: [ 89, 34, 67 ]
+    },
+    {
+      course: 'INFO 1601',
+      grades: [ 89, 34, 67 ]
+    }
+  ]
+};
+
+let sally = {
+  fname: "sally",
+  lname: "smith",
+  age: 18,
+  height: 6,
+  transcript:[
+    {
+      course: 'INFO 1601',
+      grades: [ 100, 89, 79 ]
+    }
+  ]
+};
+
+let paul = {
+  fname: "paul",
+  lname: "smith",
+  age: 18,
+  height: 6,
+  transcript:[
+    {
+      course: 'INFO 1600',
+      grades: [ 89, 34, 67 ]
+    }
+  ]
+};
+
+
+const students = [bob, sally, paul];
+
+
+function getAverageGrade(student, course){
+  for(let trans of student.transcript){
+    if (trans.course === trans){
+      let sum = 0;
+      for (let grade of trans.grades){
+        sum += grade;
+      }
+      return sum / trans.grade.length;
+  }
+  }
+  return -1;
+}
+
+
+
+console.log(getAverageGrade(bob, 'INFO 1603'));
